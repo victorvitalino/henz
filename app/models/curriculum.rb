8ecbc69c567843295ad4537ed14c1ab309871848
Victor Vitalino <vitalino.victor@gmail.com>
2015-06-19 15:05:42 -0300

    fix

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -1,6 +1,6 @@
 class Curriculum < ActiveRecord::Base
-  
-  enum :unit => [:br040]
+
+  enum :unit => ["Matriz Rio de Janeiro", "VIA 040", "BR 060", "São Paulo"]
   enum :field => ['Construção Civil', 'Manutenção e Conserva', 'Engenharia', 'RH', 'Departamento Pessoal', 'Auxiliar Administrativo','Segurança do trabalho', 'Almoxarife','Financeiro', 'Mecânica',
   'Supervisão','Serviços Gerais', 'Atendimento']
 
